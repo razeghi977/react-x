7b0bb0a0f9b74a7720359650f15bdd82e9036d83
razeghi977 <arobasm977@gmail.com>
2026-09-22 04:57:13 +0330

    version2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import Header from "../header/header";
+import Header from "../../Header/Header"
 import Slider from "../Slider/slider";
 import Products from "../Products/products";
 import Footer from "../Footer/Footer";
